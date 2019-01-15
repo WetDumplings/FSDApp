2d834151a01878bebacd89b73a2516a13d97b453
Qin Ding Han <s10180179@connect.np.edu.sg>
2019-01-15 09:41:40 +0800

    Input dummy data

--- app/src/main/java/com/danielappdev/fosterships/selectmenu.java
+++ app/src/main/java/com/danielappdev/fosterships/selectmenu.java
@@ -31,6 +31,7 @@ public class selectmenu extends AppCompatActivity {
     Button btnJoin;
     Button btnBook;
     Button btnAdminPage;
+    Button btnDum;
     EditText inviteCode;
     TextView textview2;
     Integer eventID;
@@ -48,6 +49,7 @@ public class selectmenu extends AppCompatActivity {
         inviteCode = findViewById(R.id.txtInviteCode);
         btnJoin = findViewById(R.id.btnJoinSelect);
         btnBook = findViewById(R.id.btnBook);
+        btnDum = findViewById(R.id.btnDummy);
         btnAdminPage = findViewById(R.id.btnAdminPage);
         mPref = PreferenceManager.getDefaultSharedPreferences(this);
         textview2 = findViewById(R.id.textView2);
@@ -55,7 +57,7 @@ public class selectmenu extends AppCompatActivity {
                 Settings.Secure.ANDROID_ID);
         HashMap<String, Object> Member = new HashMap<>();
         Member.put("Player",android_id);
-        defReference.child(String.valueOf("3518")).child("Teams").child("Team banana").child("Members").updateChildren(Member);
+        defReference.child("Events").child("3518").updateChildren(Member);
         Log.d("t", android_id);
         btnJoin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -89,6 +91,12 @@ public class selectmenu extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), AdminPage.class);
                 startActivity(intent);
+            }
+        });
+        btnDum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PutDummyData();
             }
         });
 
@@ -153,6 +161,45 @@ public class selectmenu extends AppCompatActivity {
 
             }
         });
+    }
+
+    public void PutDummyData(){
+        HashMap<String, Object> EventID = new HashMap<>();
+        HashMap<String, Object> EventStatus = new HashMap<>();
+        EventID.put("EventID",3518);
+        EventStatus.put("GameStatus","Ready");
+        defReference.child("3518").updateChildren(EventID);
+        defReference.child("3518").updateChildren(EventStatus);
+
+        HashMap<String, Object> TeamName = new HashMap<>();
+        HashMap<String, Object> TeamAuthcode = new HashMap<>();
+        HashMap<String, Object> NoOFAuths = new HashMap<>();
+        HashMap<String, Object> Score = new HashMap<>();
+        HashMap<String, Object> Round = new HashMap<>();
+        TeamName.put("TeamName","Team banana");
+        TeamAuthcode.put("TeamAuthCode","#frx61");
+        NoOFAuths.put("NoOfAuths",3);
+        Score.put("Score",0);
+        Round.put("Round",1);
+        defReference.child("3518").child("Teams").child("Team banana").updateChildren(TeamName);
+        defReference.child("3518").child("Teams").child("Team banana").updateChildren(TeamAuthcode);
+        defReference.child("3518").child("Teams").child("Team banana").updateChildren(NoOFAuths);
+        defReference.child("3518").child("Teams").child("Team banana").updateChildren(Score);
+        defReference.child("3518").child("Teams").child("Team banana").updateChildren(Round);
+
+        HashMap<String, Object> Member1role = new HashMap<>();
+        HashMap<String, Object> Member1round = new HashMap<>();
+        Member1role.put("Role",1);
+        Member1round.put("Round",1);
+        HashMap<String, Object> Member2role = new HashMap<>();
+        HashMap<String, Object> Member2round = new HashMap<>();
+        Member2role.put("Role",2);
+        Member2round.put("Round",1);
+        defReference.child("3518").child("Teams").child("Team banana").child("Members").child("9258d7d86f9b8932").updateChildren(Member1role);
+        defReference.child("3518").child("Teams").child("Team banana").child("Members").child("9258d7d86f9b8932").updateChildren(Member1round);
+        defReference.child("3518").child("Teams").child("Team banana").child("Members").child("8a8c1ff831636d1e").updateChildren(Member2role);
+        defReference.child("3518").child("Teams").child("Team banana").child("Members").child("8a8c1ff831636d1e").updateChildren(Member2round);
+
     }
 
 
